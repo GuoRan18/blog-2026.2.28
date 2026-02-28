0ee7459e8c04aeaf2ea44a5551dfaeab99bef781
guoran <r.92@qq.com>
2026-02-28 16:48:22 +0800

    1

--- static/js/about-photo.js
+++ static/js/about-photo.js
@@ -1,9 +1,10 @@
 //头部背景图随机显示
 (function() {
   const data = [
-    { text: '拍摄于杭州西湖', image: 'https://img.koobai.com/about.webp' },
-    { text: '拍摄于北京长城', image: 'https://img.koobai.com/about-beij.webp' },
-    { text: '拍摄于杭州千岛湖', image: 'https://img.koobai.com/about-qdh.webp' }
+    { text: '拍摄于太原植物园', image: 'https://img.010316.xyz/usr/hugo/IMG_20201107_220715.jpg' },
+    { text: '拍摄于晋阳古城', image: 'https://img.010316.xyz//usr/hugo/IMG_20211002_172824.jpg' },
+    { text: '拍摄于北京颐和园', image: 'https://img.010316.xyz///usr/Obsidian/20241119121517.png' }
+    { text: '拍摄于雁门关', image: 'https://img.010316.xyz///usr/Obsidian/photo_2024-06-17_15-13-26.jpg' }
   ];
 
   const random = data[Math.floor(Math.random() * data.length)];
