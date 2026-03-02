1159d23d7e4cbfa377e8dc7a98cb760783151f0f
guoran <r.92@qq.com>
2026-03-02 09:43:02 +0800

    更新 about-photo.js

--- static/js/about-photo.js
+++ static/js/about-photo.js
@@ -3,7 +3,7 @@
   const data = [
     { text: '拍摄于太原植物园', image: 'https://img.010316.xyz/usr/hugo/IMG_20201107_220715.jpg' },
     { text: '拍摄于晋阳古城', image: 'https://img.010316.xyz//usr/hugo/IMG_20211002_172824.jpg' },
-    { text: '拍摄于北京颐和园', image: 'https://img.010316.xyz///usr/Obsidian/20241119121517.png' }
+    { text: '拍摄于北京颐和园', image: 'https://img.010316.xyz///usr/Obsidian/20241119121517.png' },
     { text: '拍摄于雁门关', image: 'https://img.010316.xyz///usr/Obsidian/photo_2024-06-17_15-13-26.jpg' }
   ];
 
